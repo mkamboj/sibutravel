187a08ef70353fa082c25f05941e3ef235500bf8
mkamboj <malakamboj@gmail.com>
2014-12-04 22:21:23 +0530

    Devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+
+  devise_scope :user do
+    #root 'home#index', as: :authenticate_root
+    root 'devise/sessions#new'
+  end 
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
